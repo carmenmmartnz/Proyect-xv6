9b9b71772ab7dab17689acaac415df476d932dd7
carmenmmartnz <martinezperezcarmenmaria@gmail.com>
2019-11-17 17:47:24 +0100

    Primer commit

---
+++ Proyectxv6/date.c
@@ -0,0 +1,15 @@
+# include "types.h"
+# include "user.h"
+# include "date.h"
+int
+main (int argc, char *argv [])
+{
+  struct rtcdate r;
+
+  if (date(&r)){
+     printf(2, "date failed\n");
+     exit () ;
+  }
+  printf(1,"Hora: %d, Minutos: %d", r.hour, r.minute);
+		exit();
+}
